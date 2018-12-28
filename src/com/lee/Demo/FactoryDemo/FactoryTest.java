d1093e89635b7b78b0f4aec2da4ce8b7f372bddd
LeeJZe <495393214@qq.com>
2018-12-28 15:45:17 +0800

    新增静态工厂实例。

--- src/com/lee/Demo/FactoryDemo/FactoryTest.java
+++ src/com/lee/Demo/FactoryDemo/FactoryTest.java
@@ -2,8 +2,11 @@ package com.lee.Demo.FactoryDemo;
 
 public class FactoryTest {
     public static void main(String [] args){
-        HumanFactory factory = new HumanFactory();
-        Human male = factory.createHuman("male");
+        //单个工厂类
+//        HumanFactory factory = new HumanFactory();
+//        Human male = factory.createHuman("male");
+        //静态工厂类
+        Human male = HumanFactory.cereateMale();
         male.beat();
         male.eat();
         male.sleep();
--- src/com/lee/Demo/FactoryDemo/HumanFactory.java
+++ src/com/lee/Demo/FactoryDemo/HumanFactory.java
@@ -1,15 +1,29 @@
 package com.lee.Demo.FactoryDemo;
 
 public class HumanFactory {
-    public Human createHuman(String gender){
-        if(gender.equals("male")){
-            return new Male();
-        }else if (gender.equals("female")){
-            return new Female();
-        }else {
-            System.out.println("类型错误");
-            return null;
-        }
-
+    //单工厂模式
+//    public Human createHuman(String gender){
+//        if(gender.equals("male")){
+//            return new Male();
+//        }else if (gender.equals("female")){
+//            return new Female();
+//        }else {
+//            System.out.println("类型错误");
+//            return null;
+//        }
+//
+//    }
+    //静态工厂模式
+    public static Male cereateMale(){
+        return new Male();
     }
+    public static Female createFemale(){
+        return new Female();
+    }
+
+
+
+
+
+
 }
